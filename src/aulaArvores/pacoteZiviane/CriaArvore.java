38f5939342c482fd107f78aeb61aec12f92ac9e3
cassio-mendes <kassiogithub@gmail.com>
2026-04-17 19:59:28 -0300

    Adicionando código do Nivio Ziviani

---
+++ src/aulaArvores/pacoteZiviane/CriaArvore.java
@@ -0,0 +1,22 @@
+package aulaArvores.pacoteZiviane;
+
+import java.io.*;
+
+public class CriaArvore {
+
+    static void main(String[] args) throws Exception {
+        ArvoreBinaria dicionario = new ArvoreBinaria();
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        int chave = Integer.parseInt(in.readLine());
+
+        while (chave > 0) {
+            MeuItem item = new MeuItem(chave);
+            dicionario.insere(item);
+            chave = Integer.parseInt(in.readLine());
+        }
+
+        dicionario.imprime();
+    }
+
+}
---
+++ src/aulaArvores/pacoteZiviane/Item.java
@@ -0,0 +1,7 @@
+package aulaArvores.pacoteZiviane;
+
+public interface Item {
+    int compara(Item it);
+    void alteraChave(Object chave);
+    Object recuperaChave();
+}
